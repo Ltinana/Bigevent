16ba547b810be845cffc6bb8c4bee963bcb44752
“Ltinana <Ltina.xin@foxmail.com>
2021-08-24 18:32:30 +0800

    完成了文章管理相关功能的开发

---
+++ assets/js/acticle/art_list.js
@@ -0,0 +1,117 @@
+$(function() {
+    var layer = layui.layer
+    var form = layer.form
+    var laypage = layui.laypage;
+    // 定义一个查询的参数对象，将来请求数据的时候，
+    // 需要将请求参数对象提交到服务器
+    var q = {
+        pagenum: 1, // 页码值，默认请求第一页的数据
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
+    }
+
+    initTable()
+    initCate()
+
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+                template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                    // 调用渲染分页的方法
+                renderPage(res.total)
+            }
+        })
+    }
+
+    template.defaults.imports.dataFormat = function(date) {
+        var dt = new Date(date)
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + '' + hh + '-' + mm + '-' + ss
+    }
+
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                }
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                    // 通过 layui 重新渲染表单区域的UI结构
+                form.render()
+            }
+        })
+    }
+
+    $('#form-search').on('submit', function(e) {
+        e.preventDefault();
+        var cate_id = $('[name=cate_id]').val();
+        var state = $('[name=state]').val();
+        q.cate_id = cate_id;
+        q.state = state
+        initTable()
+    })
+
+    function renderPage(total) {
+        laypage.render({
+            elem: 'pageBox',
+            count: total,
+            limit: q.pagesize,
+            curr: q.pagenum,
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+            jump: function(obj, first) {
+                q.pagenum = obj.curr
+                q.pagesize = obj.limit
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+    }
+
+    $('tbody').on('click', '.btn-delete', function(res) {
+        var len = $('.btn-delete').length
+        var id = $(this).attr('data-id');
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+                    if (len === 1) {
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1;
+                    }
+                    initTable()
+                }
+            })
+
+            layer.close(index);
+        });
+    })
+
+})
